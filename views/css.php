8c50faa0856ee11064b2f40302db243566275e58
ndaru21 <muhammad.ndaru@gmail.com>
2020-01-27 00:16:13 +0700

    rapihin kodingan

--- views/css.php
+++ views/css.php
@@ -1,9 +1,6 @@
-<meta name="referrer" content="strict-origin" />
 <meta name="theme-color" content="#AFD9A7">
 <link rel="shortcut icon" href="<?= BASEURL; ?>assets/images/mn.png">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/bootstrap/css/bootstrap.css">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/aos/css/aos.css">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/style.css">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/navbar.css">
