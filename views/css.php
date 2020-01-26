cf4a71f459ffcecefa1664bcab666bf6c23f28a6
ndaru21 <muhammad.ndaru@gmail.com>
2020-01-27 00:32:53 +0700

    test 1

--- views/css.php
+++ views/css.php
@@ -1,6 +1,6 @@
 <meta name="theme-color" content="#AFD9A7">
 <link rel="shortcut icon" href="<?= BASEURL; ?>assets/images/mn.png">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/aos/css/aos.css">
+<!-- <link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/aos/css/aos.css"> -->
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/style.css">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/navbar.css">
