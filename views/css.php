10fafe8c8c0d4b161e1ca896b53dd7745bd8c349
ndaru21 <muhammad.ndaru@gmail.com>
2020-01-27 00:00:31 +0700

    rapihin assets

--- views/css.php
+++ views/css.php
@@ -1,7 +1,7 @@
 <meta name="theme-color" content="#AFD9A7">
 <link rel="shortcut icon" href="<?= BASEURL; ?>assets/images/mn.png">
-<link rel="stylesheet" href="<?= BASEURL; ?>assets/css/bootstrap.css">
-<link rel="stylesheet" href="<?= BASEURL; ?>assets/css/all.min.css">
+<link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/fontawesome/css/all.min.css">
+<link rel="stylesheet" href="<?= BASEURL; ?>assets/plugins/aos/css/aos.css">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/style.css">
-<link rel="stylesheet" href="<?= BASEURL; ?>assets/css/aos.css">
 <link rel="stylesheet" href="<?= BASEURL; ?>assets/css/navbar.css">
