96f0465156632a8892412af117c0369eedf8cdcf
ndaru21 <muhammad.ndaru@gmail.com>
2020-01-27 00:36:48 +0700

    test 2

--- views/js.php
+++ views/js.php
@@ -2,5 +2,5 @@
 <script src="//cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 <script src="<?= BASEURL; ?>assets/plugins/bootstrap/js/bootstrap.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
-<!-- <script src="<?= BASEURL; ?>assets/plugins/aos/js/aos.js"></script> -->
+<script src="<?= BASEURL; ?>assets/plugins/aos/js/aos.js"></script>
 <script src="<?= BASEURL; ?>assets/js/main.js"></script>
